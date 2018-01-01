de4821e7903a189079f8c58c8dccd8c199bc1c95
Paul <pmorris2012@fau.edu>
2017-12-31 23:11:24 -0500

    refactored oscillator into single class

--- oscillator.py
+++ oscillator.py
@@ -3,35 +3,26 @@ import numpy as np
 from param import _Param
 
 
-class _Oscillator(_Param):
+class Oscillator(_Param):
     def __init__(self, params):
-        self._setup_param_list(["start", "duration", "amplitude", "frequency", "sample_rate"])
-        super().__init__(params)
-        self.buff = np.arange(int(self.sample_rate * self.duration)).astype(np.float32)
-        
-
-class OscSine(_Oscillator):
-    def __init__(self, params):
-        super().__init__(params)
-        self.buff = np.sin(2 * np.pi * self.buff * self.frequency / self.sample_rate)
-
-class OscSawtooth(_Oscillator):
-    def __init__(self, params):
-        super().__init__(params)
-        self.buff = np.mod(1 + 2 * self.buff * self.frequency / self.sample_rate, 2) - 1
-
-class OscTriangle(_Oscillator):
-    def __init__(self, params):
-        super().__init__(params)
-        self.buff = 2 * np.abs(np.mod(-0.5 + 2 * self.buff * self.frequency / self.sample_rate, 2) -1) - 1
-
-class OscSquare(OscSawtooth, _Param):
-    def __init__(self, params):
+        self._setup_param_list(["osc_type", "start", "duration", "amplitude", "frequency", "sample_rate"])
         self._setup_opt_param_list(["duty_cycle"])
         super().__init__(params)
-        
+        self.buff = np.arange(int(self.sample_rate * self.duration)).astype(np.float32)
+        self.buff = getattr(self, "fn_" + self.osc_type)(self.buff)
+
+    def fn_sine(self, buff):
+        return np.sin(2 * np.pi * buff * self.frequency / self.sample_rate)
+
+    def fn_sawtooth(self, buff):
+        return np.mod(1 + 2 * buff * self.frequency / self.sample_rate, 2) - 1
+
+    def fn_triangle(self, buff):
+        return 2 * np.abs(np.mod(-0.5 + 2 * buff * self.frequency / self.sample_rate, 2) -1) - 1
+
+    def fn_square(self, buff):
         assert self.duty_cycle >= 0 and self.duty_cycle <= 1
-        self.buff = np.sign(self.buff + 1 - 2 * self.duty_cycle)
+        return np.sign(self.fn_sawtooth(buff) + 1 - 2 * self.duty_cycle)
 
     def _set_opt_param_vals(self, params):
         if not self._is_opt_param_set("duty_cycle", params):
